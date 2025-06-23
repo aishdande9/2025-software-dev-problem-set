052477c063501829801fd9d6dfd430039fbfb490
aishdande9 <aish.dande@gmail.com>
2025-06-23 11:02:32 -0500

    3Sum

--- solutions/medium/3sum.js
+++ solutions/medium/3sum.js
@@ -3,11 +3,19 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    for(let i = 0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            for(let k=j;k<nums.length;k++){
-
-            }
-        }
-    }
-};
+    const res = new Set();
+          nums.sort((a, b) => a - b);
+          for (let i = 0; i < nums.length; i++) {
+              for (let j = i + 1; j < nums.length; j++) {
+                  for (let k = j + 1; k < nums.length; k++) {
+                      if (nums[i] + nums[j] + nums[k] === 0) {
+                          res.add(JSON.stringify([nums[i], nums[j], nums[k]]));
+                      }
+                  }
+              }
+          }
+          return Array.from(res).map(item => JSON.parse(item));
+      };
+  
+  
+  
