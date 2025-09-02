cb78e964c83edd6c73d5f14faea457d375f8282d
aishdande9 <aish.dande@gmail.com>
2025-09-02 12:23:06 -0500

    problem solved

--- solutions/JavaSolutions/ValidSudoku.java
+++ solutions/JavaSolutions/ValidSudoku.java
@@ -1,2 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ValidSudoku {
-}
+    public static void main(String[] args) {
+
+            Scanner scan = new Scanner(System.in);
+        System.out.println("Enter word?");
+            String s = scan.nextLine();
+
+            String[] word = s.split("\\s'");
+        for (String words : word) {
+            System.out.println(words);
+        }
+            // Write your code here.
+            scan.close();
+        }
+    }
+
+
